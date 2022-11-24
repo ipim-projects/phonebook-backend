ea2bba87ae81ee9e02213fc8230051a9886b99ef
belaniv <igor15nnn@gmail.com>
2022-11-24 23:09:46 +0300

    Переделана выгрузка на /export/?type=employee-job или type=phonebook

--- src/main/java/ru/ipim/phonebook/controller/ExportController.java
+++ src/main/java/ru/ipim/phonebook/controller/ExportController.java
@@ -2,13 +2,9 @@ package ru.ipim.phonebook.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
-import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import net.minidev.json.JSONObject;
 import lombok.extern.slf4j.Slf4j;
@@ -17,20 +13,16 @@ import liquibase.pro.packaged.T;
 import ru.ipim.phonebook.repository.EmployeeRepository;
 import ru.ipim.phonebook.repository.JobRepository;
 
-import ru.ipim.phonebook.model.Employee;
 import ru.ipim.phonebook.model.EmpExportType0;
 import ru.ipim.phonebook.model.EmpExportType1;
 import ru.ipim.phonebook.model.EmpExportType2;
-import ru.ipim.phonebook.model.StatCompanys;
 import ru.ipim.phonebook.util.DirSettings;
 
-import javax.validation.Valid;
-import javax.validation.constraints.Min;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 import java.io.FileWriter;
 import java.io.PrintWriter;
+import java.util.Objects;
 
 @RestController
 @Slf4j
@@ -48,74 +40,69 @@ public class ExportController {
 
     // Выгрузка тип 1 : Телефонная книга с сортировкой по фамилии и имени
     // поля : Фамилия, Имя, Рабочий телефон, Мобильный телефон, e-mail, Дата рождения
-    @GetMapping("/export1")
-    @ApiOperation("Экспорт всех записей, тип экспорта 1. Телефонная книга с сортировкой по фамилии и имени")
-    public @ResponseBody List<JSONObject>  exportType1() {
-        List<EmpExportType1> entityList = employeeRepository.exportType1WithJPQL();
-
-        List<JSONObject> entities = new ArrayList<JSONObject>();
-        for (EmpExportType1 n : entityList) {
-            JSONObject entity = new JSONObject();
-            entity.put("lastName", n.getLastName());
-            entity.put("firstName", n.getFirstName());
-            entity.put("birthdate", n.getBirthdate().toString());
-            entity.put("mobilePhone", n.getMobilePhone());
-            entity.put("workPhone", n.getWorkPhone());
-            entity.put("email", n.getEmail());
-            entities.add(entity);
-        }
-
-        // сгенерировать имя файла в каталоге для экспорта
-        String filename = settings.getOutFileName().toString();
-
-        // файл данных
-        String filetxt = filename + ".txt";
-        try (PrintWriter out = new PrintWriter(new FileWriter(filetxt))) {
-            out.write(entities.toString());
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        // файл флаг пустой
-        String fileready = filename + ".ready";
-        try (PrintWriter out = new PrintWriter(new FileWriter(fileready))) {
-            out.write("");
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        log.info("Выгрузка тип 1. Телефонная книга с сортировкой по фамилии и имени, количество записей: {}", entityList.stream().count());
-        log.info("Подготовлены следующие данные: {}", entities);
-        return entities;
-    }
-
+    // Пример обращения: type=phonebook
+    //  http://localhost:8080/export/?type=phonebook&startdate=2022-11-09&enddate=2022-12-12
+    //
     // Выгрузка тип 2 : сотрудники с привязкой к местам работы.
     // Поля: фамилия,имя, мобильный телефон, email +  Наименование организации, Должность, Рабочий адрес.
-    @GetMapping("/export2")
-    @ApiOperation("Экспорт всех записей, тип экспорта 2")
-    public @ResponseBody List<JSONObject>  exportType2() {
-        List<EmpExportType2> entityList = employeeRepository.exportType2WithJPQL();
+    // Пример обращения: type=employee-job
+    //  http://localhost:8080/export/?type=employee-job&startdate=2022-11-09&enddate=2022-12-12
+    @GetMapping("/export/")
+    @ApiOperation("Экспорт записей. phonebook = тип выгрузки 1, employee-job = тип выгрузки 2 ")
+    public @ResponseBody List<JSONObject>  exportTypesOneAndTwo(@RequestParam String type) {
+      // результат формирования экспорта будет здесь:
+      List<JSONObject> entities = new ArrayList<JSONObject>();
 
-        List<JSONObject> entities = new ArrayList<JSONObject>();
-        for (EmpExportType2 n : entityList) {
-            JSONObject entity = new JSONObject();
-            entity.put("lastName", n.getLastName());
-            entity.put("firstName", n.getFirstName());
-            entity.put("mobilePhone", n.getMobilePhone());
-            entity.put("email", n.getEmail());
-            entity.put("company", n.getCompany());
-            entity.put("jobTitle", n.getJobTitle());
-            entity.put("address", n.getAddress());
-            entities.add(entity);
-        }
+      if (Objects.equals(type, "phonebook")) {
+          log.info("Запуск выгрузки тип 1. Телефонная книга");
+          List<EmpExportType1> entityList = employeeRepository.exportType1WithJPQL();
 
-        // сгенерировать имя файла в каталоге для экспорта
+          for (EmpExportType1 n : entityList) {
+              JSONObject entity = new JSONObject();
+              entity.put("lastName", n.getLastName());
+              entity.put("firstName", n.getFirstName());
+              entity.put("birthdate", n.getBirthdate().toString());
+              entity.put("mobilePhone", n.getMobilePhone());
+              entity.put("workPhone", n.getWorkPhone());
+              entity.put("email", n.getEmail());
+              entities.add(entity);
+          }
+          log.info("Выгрузка тип 1. Телефонная книга с сортировкой по фамилии и имени, количество записей: {}", (long) entityList.size());
+
+      }
+      else if (Objects.equals(type, "employee-job")) {
+          log.info("Запуск выгрузки тип 1. Отдельные поля Телефонной книги + поля из Место работы");
+          List<EmpExportType2> entityList = employeeRepository.exportType2WithJPQL();
+
+          for (EmpExportType2 n : entityList) {
+              JSONObject entity = new JSONObject();
+              entity.put("lastName", n.getLastName());
+              entity.put("firstName", n.getFirstName());
+              entity.put("mobilePhone", n.getMobilePhone());
+              entity.put("email", n.getEmail());
+              entity.put("company", n.getCompany());
+              entity.put("jobTitle", n.getJobTitle());
+              entity.put("address", n.getAddress());
+              entities.add(entity);
+          }
+          log.info("Выгрузка тип 2. Телефонная книга с сортировкой по фамилии и имени + поля из Место работы, количество записей: {}", (long) entityList.size());
+
+      }
+      else {
+          log.info("Выгрузка неизвестного типа. Переданное в URL значение type= '{}' не может быть обработано. ", type);
+          log.info("Допустимые значения: type=phonebook или type=employee-job");
+          return entities;
+      }
+      log.info("Подготовлены следующие данные: {}", entities);
+
+        // сгенерировать имя файла в каталоге для экспорта, если файл уже есть он будет перезаписан
         String filename = settings.getOutFileName().toString();
 
         // файл данных
         String filetxt = filename + ".txt";
         try (PrintWriter out = new PrintWriter(new FileWriter(filetxt))) {
             out.write(entities.toString());
+            log.info("Записан файл данных: {}", filetxt);
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -124,17 +111,16 @@ public class ExportController {
         String fileready = filename + ".ready";
         try (PrintWriter out = new PrintWriter(new FileWriter(fileready))) {
             out.write("");
+            log.info("Записан файл-флаг: {}", filetxt);
         } catch (Exception e) {
             e.printStackTrace();
         }
 
-        log.info("Выгрузка тип 2. Телефонная книга с сортировкой по фамилии и имени, количество записей: {} - {}", entityList.stream().count());
-        log.info("Подготовлены следующие данные: {}", entities);
         return entities;
     }
 
     // Все записи талицы Employee дополненные местом работы, должностью, адресом
-    @GetMapping("/export3")
+    @GetMapping("/export-test")
     @ApiOperation("Получение списка всех записей: join employee + job. Сортировка по Фамилии и имени")
     public @ResponseBody List<JSONObject> findEmployeesJobs() {
         List<EmpExportType0> entityList = employeeRepository.exportAllWithJPQL();
